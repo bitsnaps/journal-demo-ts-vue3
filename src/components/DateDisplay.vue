2f7a3c0b6edccf39f413f7913ff181f40150597f
Ibrahim.H <bitsnaps@yahoo.fr>
2022-10-02 12:24:34 +0100

    example of using options api

--- src/components/DateDisplay.vue
+++ src/components/DateDisplay.vue
@@ -1,17 +1,39 @@
 <script lang="ts">
-import formatRelative from "date-fns/formatRelative";
-import { defineComponent } from "vue";
+import formatRelative from "date-fns/formatRelative"
+import { defineComponent /*, type PropType */ } from "vue"
+// import type Entry from "@/types/Entry"
+
+// This component uses the old way of Options API instead of Composition API
+
+// defineComponent allows typescript to detect types
 export default defineComponent({
   props: {
     date: { type: Date, required: true },
+    // We can't use type declaration if we want to use a type (e.g. Entry)
+    // date: { type: Entry, required: true },
+    // In this case, we can use Object casted to PropType from "vue"
+    // date: { type: Object as PropType<Entry>, required: true },
   },
+/*  emits: {
+    doSomething(payload: Entry){
+      // return payload.body && payload.createdAt // etc..
+      return true
+    }
+  }, */
   computed: {
-    formatted() {
+    //formatted() here can by typed
+    formatted() /* : string */ {
       return formatRelative(this.date, Date.now());
     },
   },
-});
+/*  mounted() {
+    this.formatted
+  }
+*/
+})
+
 </script>
+
 <template>
   <div>{{ formatted }}</div>
 </template>
--- src/components/EntryCard.vue
+++ src/components/EntryCard.vue
@@ -43,6 +43,7 @@ const props = defineProps({
       <div class="entry-text">{{ entry.body }}</div>
     </div>
     <div class="entry-footer">
+      <!-- <DateDisplay :date="entry.createdAt" class="mr-2" @do-something="$event.body" /> -->
       <DateDisplay :date="entry.createdAt" class="mr-2" />
       |
       <span class="ml-2">{{ user?.username || "anonymous"}}</span>
